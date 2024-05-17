893cd580be4cc89b6b27419e19612b2b0394d0b0
andersonaug <andersonaug@ecelepar21120.celeparsl.smb4.parana>
2024-05-16 22:03:30 -0300

    Desafio.

--- contabanco/src/contabanco/ContaTerminal.java
+++ contabanco/src/contabanco/ContaTerminal.java
@@ -1,10 +1,32 @@
 package contabanco;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		System.out.println("Olá");
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+		System.out.println("Por favor, digite a Agência!");
+		String agencia = scanner.next();
+		
+		System.out.println("Por favor, digite o Número da Conta!");
+		int numeroConta = scanner.nextInt();
+		
+		System.out.println("Por favor, digite o Nome do Cliente!");
+		String nomeCliente = scanner.next();
+		
+		System.out.println("Por favor, digite o Saldo da Conta!");
+		double saldoConta = scanner.nextDouble();
+		
+		System.out.print("Olá " + nomeCliente);
+		System.out.print(", obrigado por criar uma conta em nosso banco, sua agência é " + agencia);
+		System.out.print(", conta " + numeroConta);
+		System.out.print(" e seu saldo " + saldoConta);
+		System.out.print(" já está disponível para saque.");
+		
+		scanner.close();
 	}
 
 }
